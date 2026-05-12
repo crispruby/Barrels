90a749e85d1e3aa21550efbbd7be1e580e01c61d
Daniel Rozek <drobpc@gmail.com>
2026-05-11 17:16:14 -0700

    Refactor SpawnManager to use GameConfig values

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,71 +1,71 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class SpawnManager : MonoBehaviour
 {
-  public GameObject[] enemies;
-  public GameObject[] powerups;
-  private float zEnemySpawn = 14.0f;
-  private float xSpawnRange = 5.0f;
-  private float ySpawn = 0.65f;
-  private float yPowerup = 1;
+    public GameObject[] enemies;
+    public GameObject[] powerups;
+    private float zEnemySpawn = GameConfig.SpawnZ;
+    private float xSpawnRange = GameConfig.SpawnXRange;
+    private float ySpawn = GameConfig.SpawnEnemyY;
+    private float yPowerup = GameConfig.SpawnPowerupY;
 
-  private float powerupSpawnTime = 10.0f;
-  private float enemySpawnTime = 0.75f;
-  private float startDelay = 1.0f;
-  private bool shouldSpawn = false;
-  // Start is called before the first frame update
-  void Start()
-  {
+    private float powerupSpawnTime = GameConfig.SpawnPowerupTime;
+    private float enemySpawnTime = GameConfig.SpawnEnemyTime;
+    private bool shouldSpawn = false;
 
-  }
-  // Update is called once per frame
-  void Update()
-  {
-
-  }
-  public void SetSpawnTimes(float powerupTime, float enemyTime)
-  {
-    powerupSpawnTime = powerupTime;
-    enemySpawnTime = enemyTime;
-  }
-  public void StartSpawning()
-  {
-    shouldSpawn = true;
-    InvokeRepeating("SpawnEnemy", startDelay, enemySpawnTime);
-    InvokeRepeating("SpawnPowerup", startDelay, powerupSpawnTime);
-  }
-  public void StopSpawning()
-  {
-    shouldSpawn = false;
-    CancelInvoke("SpawnEnemy");
-    CancelInvoke("SpawnPowerup");
-  }
-  void SpawnEnemy()
-  {
-    if (shouldSpawn)
+    public void SetSpawnTimes(float powerupTime, float enemyTime)
     {
-      float randomX = Random.Range(-xSpawnRange, xSpawnRange);
-      int randomIndex = Random.Range(0, enemies.Length);
-      Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
-      Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+        powerupSpawnTime = powerupTime;
+        enemySpawnTime = enemyTime;
     }
-  }
-  void SpawnPowerup()
-  {
-    if (shouldSpawn)
+    public void StartSpawning()
     {
-      float randomX = Random.Range(-xSpawnRange, xSpawnRange);
-      int randomIndex = Random.Range(0, powerups.Length);
-      Vector3 spawnPos = new Vector3(randomX, yPowerup, zEnemySpawn);
-      Instantiate(powerups[randomIndex], spawnPos, powerups[randomIndex].gameObject.transform.rotation);
+        if (shouldSpawn)
+        {
+            return;
+        }
+
+        shouldSpawn = true;
+        InvokeRepeating(nameof(SpawnEnemy), GameConfig.SpawnStartDelay, enemySpawnTime);
+        InvokeRepeating(nameof(SpawnPowerup), GameConfig.SpawnStartDelay, powerupSpawnTime);
     }
-  }
-  public void AdjustEnemySpawnTime(float adjustment)
-  {
-    enemySpawnTime = Mathf.Max(0.2f, enemySpawnTime + adjustment); // Ensure spawn time doesn't go below 0.1
-    CancelInvoke("SpawnEnemy"); // Cancel the current spawn
-    InvokeRepeating("SpawnEnemy", startDelay, enemySpawnTime); // Reinvoke with the new time
-  }
+    public void StopSpawning()
+    {
+        shouldSpawn = false;
+        CancelInvoke(nameof(SpawnEnemy));
+        CancelInvoke(nameof(SpawnPowerup));
+    }
+    void SpawnEnemy()
+    {
+        if (!shouldSpawn)
+        {
+            return;
+        }
+
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, enemies.Length);
+        Vector3 spawnPos = new Vector3(randomX, ySpawn, zEnemySpawn);
+        Instantiate(enemies[randomIndex], spawnPos, enemies[randomIndex].gameObject.transform.rotation);
+    }
+    void SpawnPowerup()
+    {
+        if (!shouldSpawn)
+        {
+            return;
+        }
+
+        float randomX = Random.Range(-xSpawnRange, xSpawnRange);
+        int randomIndex = Random.Range(0, powerups.Length);
+        Vector3 spawnPos = new Vector3(randomX, yPowerup, zEnemySpawn);
+        Instantiate(powerups[randomIndex], spawnPos, powerups[randomIndex].gameObject.transform.rotation);
+    }
+    public void AdjustEnemySpawnTime(float adjustment)
+    {
+        enemySpawnTime = Mathf.Max(GameConfig.MinEnemySpawnTime, enemySpawnTime + adjustment);
+
+        if (shouldSpawn)
+        {
+            CancelInvoke(nameof(SpawnEnemy));
+            InvokeRepeating(nameof(SpawnEnemy), GameConfig.SpawnStartDelay, enemySpawnTime);
+        }
+    }
 }
